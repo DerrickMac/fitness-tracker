06c32cec1c607e594e3f37c7cb1d7fa73899b90e
Derrick <dsoriano109@gmail.com>
2025-01-06 18:23:03 -0800

    implement user profiles

--- app/routes.py
+++ app/routes.py
@@ -56,3 +56,10 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    user_workouts = db.session.scalars(sa.select(Workout).where(Workout.owner == current_user)).all()
+    return render_template('user.html', user=user, workouts=user_workouts)
