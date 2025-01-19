c3812a05891f261df3aad3c8e31f75b96c25271f
DerrickMac <dmsoftwarestore@gmail.com>
2025-01-19 14:39:34 -0800

    format timestamps with moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 from logging.handlers import RotatingFileHandler
 import os
 import logging
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+moment = Moment(app)
 
 from app import routes, models, errors
 
