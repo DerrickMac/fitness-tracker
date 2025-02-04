114f8ceb7c86b10a7661febecfcd80e21f0f11df
Derrick <dsoriano109@gmail.com>
2025-02-04 15:11:45 -0800

    GCP port configuration/PostgresSQL set up

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:slim
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-RUN pip install gunicorn pymysql cryptography
+RUN pip install gunicorn
 
 COPY app app
 COPY migrations migrations
@@ -11,5 +11,5 @@ RUN chmod a+x boot.sh
 
 ENV FLASK_APP=fitness-tracker.py
 
-EXPOSE 5000
+EXPOSE 8080
 ENTRYPOINT ["./boot.sh"]
--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    WORKOUTS_PER_PAGE = 3
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    WORKOUTS_PER_PAGE = 5
